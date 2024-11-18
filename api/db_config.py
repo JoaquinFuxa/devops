1a1c250f316eceb276cd726d5440131615724e02
Pedro Joaquín Fuxa <joaquinfuxa@hotmail.com>
2024-11-18 15:21:47 -0300

    Contenedores funcionando

--- api/db_config.py
+++ api/db_config.py
@@ -9,9 +9,4 @@ app.config['MYSQL_DATABASE_DB'] =  os.environ.get('MYSQL_DATABASE_DB')
 app.config['MYSQL_DATABASE_HOST'] =  os.environ.get('MYSQL_DATABASE_HOST')  or 'localhost'
 app.config['MYSQL_DATABASE_PORT'] =  int(os.environ.get('MYSQL_DATABASE_PORT')) or 3306
 print(app.config)
-print("DB User:", os.environ.get('MYSQL_DATABASE_USER'))
-print("DB Password:", os.environ.get('MYSQL_DATABASE_PASSWORD'))
-print("DB Host:", os.environ.get('MYSQL_DATABASE_HOST'))
-print("DB Port:", os.environ.get('MYSQL_DATABASE_PORT'))
-print("DB:", os.environ.get('MYSQL_DATABASE_DB'))
 mysql.init_app(app)
